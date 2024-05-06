1733daf623e20c02eb408579a2fed14e9476ff35
ramaadi <17980909+rama-adi@users.noreply.github.com>
2024-05-06 16:51:01 +0700

    Initial Commit

---
+++ MusicGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicGame
+//
+//  Created by Rama Adi Nugraha on 06/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicGame/MusicGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicGameApp.swift
+//  MusicGame
+//
+//  Created by Rama Adi Nugraha on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MusicGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
